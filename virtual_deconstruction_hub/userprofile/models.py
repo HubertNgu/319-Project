4c3043db296828896ab21cbe9cae66837f1bc656
unknown <dennislzf@gmail.com>
2013-03-09 15:52:14 -0800

    Commiting work done by Dennis Saving Dennis' code

---
+++ virtual_deconstruction_hub/userprofile/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length = 20)
+    firstname = models.CharField(max_length = 50)
+    lastname = models.CharField(max_length = 50)
+    phoneno = models.CharField(max_length = 10)
+    address = models.CharField(max_length = 50)
+    city = models.CharField(max_length = 50)
+    country = models.CharField(max_length = 50)
+    province = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 500)
